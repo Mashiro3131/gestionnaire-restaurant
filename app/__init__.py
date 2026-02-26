dca09163f366c2bd1524dfa9287ef3bc57628691
Nico Mengisen <mengisennico@gmail.com>
2026-02-26 16:42:22 +0100

    Update __init__ for /app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,16 @@
 # Crée l'application Flask et configure les routes, les modèles de données, etc.
 
+
 from flask import Flask
 from config import Config
 
 def create_app():
-    pass
-    # app = Flask(__name__)
-    # app.config.from_object(Config)
-    # # Importer les routes
-    # from app.auth import auth as auth_blueprint
-    # from app.main import auth as main_blueprint
-    # from app.admin import auth as admin_blueprint
-    
-    # app.register_blueprint(auth_blueprint)
-    # app.register_blueprint(main_blueprint)
-    # app.register_blueprint(admin_blueprint)
-    
-    # return app
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Importer les routes
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
