71d1eac1f48a2c14ab87ce2abae9853779600adb
Nico Mengisen <mengisennico@gmail.com>
2026-02-26 16:40:51 +0100

    Add configuration settings for application

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 # Configuration de l'application
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Charger
+
 class Config:
-    pass
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    DEBUG = os.getenv('DEBUG') == 'True'
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+    PORT = int(os.getenv('PORT', 5050))
+    
