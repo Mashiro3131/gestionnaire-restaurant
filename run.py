5d937a45232313ba3131be9d4e60f04158b3bf79
Nico Mengisen <mengisennico@gmail.com>
2026-02-26 16:41:13 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 # Lanceur du serveur de développement
 # .venv\Scripts\activate  
 
+    
 from app import create_app
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
     
