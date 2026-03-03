88f6af057f6d54e681fa6128dc84805cde9c5a85
Nico Mengisen <mengisennico@gmail.com>
2026-03-03 13:24:32 +0100

    Update config.py for flask-babel languages

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 # Configuration de l'application
-
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # Charger
+load_dotenv() # Charger le .env
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
@@ -11,4 +10,6 @@ class Config:
     DEBUG = os.getenv('DEBUG') == 'True'
     ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
     PORT = int(os.getenv('PORT', 5050))
-    
+    LANGUAGES = os.getenv('LANGUAGES', '').split(',')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
