ce8aab741e96c9158a6e8878278d5d14d8522967
Nico Mengisen <mengisennico@gmail.com>
2026-03-16 14:15:40 +0100

    Update __init__.py

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
-auth_bp = Blueprint('auth', __name__) # Change 'auth' par 'main' ou 'admin' selon le dossier
+auth = Blueprint('auth', __name__) # Change 'auth' par 'main' ou 'admin' selon le dossier
 from app.auth import routes
