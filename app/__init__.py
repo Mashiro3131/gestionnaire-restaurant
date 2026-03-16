06f50cb7bf55d633ed1e643652978d2afea27e1e
Nico Mengisen <mengisennico@gmail.com>
2026-03-16 14:11:27 +0100

    Update __init__.py file structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
-# Crée l'application Flask et configure les routes, les modèles de données, etc.
-from flask import Flask, g, request
+"""
+Crée le backbone du projet Flask en configurant les routes, les modèles de données, etc.
+"""
+
+from flask import Flask,request, g 
 from app.extensions import db, login_manager, babel
 from config import Config
-
-def get_timezone():
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.timezone
 
 
 def create_app():
@@ -18,19 +16,30 @@ def create_app():
     login_manager.init_app(app)
     babel.init_app(app, locale_selector=get_locale, timezone_selector=get_timezone)
 
-    # Importer les routes
-    from app.main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    
+    app.route("/")(lambda: "Hello, World!")  # Route de test pour vérifier que l'app fonctionne
+
+    """ Importer les routes """
+    # Main Blueprint
+    from app.main import main as main_bp
+    app.register_blueprint(main_bp)
+
+    # Admin Blueprint
+    from app.admin import admin as admin_bp
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+
+    # Authentication Blueprint
+    from app.auth import auth as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     return app
 
-# Fonction pour déterminer la langue de l'utilisateur
-def get_locale():
-    # if a user is logged in, use the locale from the user settings
+def get_timezone():
     user = getattr(g, 'user', None)
     if user is not None:
-        return user.locale
-    # otherwise try to guess the language from the user accept
-    # header the browser transmits.  We support de/fr/en in this
-    # example.  The best match wins.
-    return request.accept_languages.best_match(['fr', 'de', 'en'])
+        return user.timezone
+
+def get_locale():
+    if not g.get('lang_code', None):
+        g.lang_code = request.accept_languages.best_match(Config.LANGUAGES)
+    return g.lang_code
+    # https://medium.com/@ishanthari96/create-a-multilingual-web-application-using-python-flask-babel-2e4a36471dc3
