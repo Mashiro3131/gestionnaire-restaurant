9afa54124df3d4b67cdcd7e879be935341b2d8a2
Nico Mengisen <mengisennico@gmail.com>
2026-03-20 16:00:39 +0100

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,11 @@
 # Extensions (SQLAlchemy, LoginManager, etc.)
 
+import colorama
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+colorama.init(autoreset=True)
+
 #from flask_babel import Babel
 
 db = SQLAlchemy()
