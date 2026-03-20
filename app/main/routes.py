49112a806ba36d097e376e27e4b1959abc8e16a6
Nico Mengisen <mengisennico@gmail.com>
2026-03-20 16:04:21 +0100

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,37 @@
-from app.main import main
-@main.route('/')
+from flask import render_template, abort
+from app.main import main as main_bp
+from app.models import *
+from jinja2 import TemplateNotFound
+#from flask_babel import gettext as _
+# https://realpython.com/flask-blueprint/#what-a-flask-application-looks-like
+
+
+
+@main_bp.route('/')
 def index():
-    return "<h1>Bienvenue sur la page d'accueil du restaurant !</h1>"
+
+    # dishes = Dish.query.filter_by(is_active=True).all()
+    try:
+        return render_template('main/index.html', title=('Home'))
+    except TemplateNotFound:
+        abort(404)
+
+
+
+@main_bp.route('/menu')
+def menu():
+    dishes = Dish.query.all()
+    return render_template('main/menu.html', dishes=dishes)
+
+
+# Page Navbar Order
+@main_bp.route('/order')
+def order():
+    order = Order.query.all()
+    return render_template('main/order.html', order=order)
+
+# Page Navbar Contact
+@main_bp.route('/contact')
+def contact():
+    # Route simple pour la page de contact
+    return render_template('main/contact.html')
